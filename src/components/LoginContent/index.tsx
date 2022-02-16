1018f83a5b117fb27da4305890c9632f0d5f3ade
Aryaman Singh <aryaman.singh.2012@gmail.com>
2022-02-15 21:41:30 -0500

    v7 added login page validation

--- src/components/LoginContent/index.tsx
+++ src/components/LoginContent/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import logo from "../../assets/logo.svg";
 
 import './index.css';
@@ -7,26 +8,53 @@ type Props = {
     setModal: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
+const username = "username";
+const password = "password";
+
 function LoginContent({ setIsLoggedIn, setModal }: Props) {
+    // In an actual app, I would use <form> and a form library like ReactHookForms, Formik or Informed
+    const [usernameInput, setUsernameInput] = useState<string>("");
+    const [passwordInput, setPasswordInput] = useState<string>("");
+    const [wrongPassword, setWrongPassword] = useState<boolean>(false);
+
     return (
         <div className="login-main">
             <img className="login-logo" src={logo} alt="Logo" />
 
             <label className="login-label" htmlFor="login"> Username: </label>
-            <input className="login-input" type="text" id="login" name="login" />
+            <input
+                className="login-input"
+                type="text"
+                id="login"
+                name="login"
+                onBlur={event => setUsernameInput(event.target.value)}
+            />
             <label className="login-label" htmlFor="password"> Password: </label>
-            <input className="login-input" type="password" id="password" name="password" />
+            <input
+                className="login-input"
+                type="password"
+                id="password"
+                name="password"
+                onBlur={event => setPasswordInput(event.target.value)}
+            />
             <div>
                 <button
+                    type="submit"
                     className="login-button"
                     onClick={() => {
-                        setIsLoggedIn(true);
-                        setModal(false);
+                        if (usernameInput === username && passwordInput === password) {
+                            setIsLoggedIn(true);
+                            setWrongPassword(false);
+                            setModal(false);
+                        } else {
+                            setWrongPassword(true);
+                        }
                     }}
                 >
                     Log In
                 </button>
             </div>
+            {wrongPassword && <p className="login-invalid-credentials"> Invalid Credentials. <br /> Hint: It's 'username' and 'password' 😉 </p>}
         </div>
     )
 }
